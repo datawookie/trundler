2609646850d519e9af1dfd1758bf5b5415e55d96
Andrew Collier <andrew@exegetic.biz>
2020-02-23 05:48:23 +0200

    Change font and position of name

--- inst/hex/retail-hex.R
+++ inst/hex/retail-hex.R
@@ -3,29 +3,32 @@ library(showtext)
 
 # Load Google font.
 #
-font_add_google("Roboto Slab", "roboto_slab")
+font_add_google("Raleway", "raleway")
 showtext_auto()
 
 sticker(here::here("inst/hex/cash-register.png"),
         # Image
-        s_x = 0.95,
-        s_y = 1.15,
-        s_width = 0.6,
-        s_height = 0.6,
+        s_x = 0.85,
+        s_y = 1.10,
+        s_width = 0.675,
+        s_height = 0.675,
         # Package name
         package = "retail",
-        p_size =48,
-        p_y = 0.45,
+        p_size = 64,
+        p_x = 1.350,
+        p_y = 0.425,
         p_color = "#000000",
-        p_family = "roboto_slab",
+        p_family = "raleway",
         # Hex
-        h_fill = "#ffffff",
+        h_fill = "#cccccc",
         h_color = "#000000",
         # Spotlight
-        spotlight = FALSE,
-        # l_y = 0.45,
+        # spotlight = TRUE,
+        # l_x = 0.85,
+        # l_y = 1.10,
         # l_alpha = 0.4,
         # Output
         filename = here::here("man/figures/retail-hex.png"),
-        dpi = 600
+        dpi = 600,
+        angle = 30
 )
