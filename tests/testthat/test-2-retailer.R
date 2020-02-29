35ee716a53742fc041c600b0e14a90e42f6030bf
Andrew B. Collier <andrew@exegetic.biz>
2020-02-29 15:32:06 +0000

    Add more tests

--- tests/testthat/test-2-retailer.R
+++ tests/testthat/test-2-retailer.R
@@ -15,3 +15,11 @@ test_that("specific retailer", {
 
   expect_equal(names(first_retailer), c("id", "name", "url", "currency"))
 })
+
+test_that("retailer products", {
+  expect_error(retailer_products(1), NA)
+
+  first_retailer_products <- retailer_products(1)
+
+  expect_equal(names(first_retailer_products), c("id", "name", "brand", "sku"))
+})
--- tests/testthat/test-3-product.R
+++ tests/testthat/test-3-product.R
@@ -1,1 +1,17 @@
 context("product")
+
+test_that("product", {
+  expect_error(product(1), NA)
+
+  first_product <- product(1)
+
+  expect_equal(names(first_product), c("id", "retailer_id", "url", "name", "brand", "sku", "barcodes"))
+})
+
+test_that("product prices", {
+  expect_error(product_prices(1), NA)
+
+  first_product_prices <- product_prices(1)
+
+  expect_equal(names(first_product_prices), c("product_id", "time", "price", "price_promotion", "available"))
+})
