ef1cee81387f9eb827dcc0bf75ebb036d8012d19
Andrew B. Collier <andrew@exegetic.biz>
2020-03-22 05:20:17 +0000

    Move scripts to sub-folder

--- scripts/test.R
+++ scripts/test.R
@@ -10,9 +10,19 @@ retailer()
 set_api_key(API_KEY)
 get_api_key()
 
+retailer_products(28)
+
+product_filter(name = "impossible_name")
+
+retailers = retailer()
+
+illy_coffee = product_filter(name = "coffee", brand = "Illy")
+
 retailer_products(9, verbose = TRUE)
+retailer_products(15, verbose = TRUE)
 retailer_products(9, name = "nescafe")
 retailer_products(10, brand = "nivea")
+retailer_products(1, brand = "Bosch", name = "CAT")
 product(546082) %>% select(-retailer_id, -url)
 product_prices(546082)
 product(531589) %>% select(-retailer_id, -brand, -url)
