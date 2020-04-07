56cb2e50d7bc2fbcd8d642aba12da6fd2a7e6db8
Andrew B. Collier <andrew@exegetic.biz>
2020-04-07 03:46:09 +0000

    Add stress testing script

---
+++ scripts/stress-test.R
@@ -0,0 +1,29 @@
+# This script is for (internal) API testing only. Please don't run it!
+
+library(retail)
+library(dplyr)
+
+API_KEY <- Sys.getenv("RETAIL_KEY")
+
+set_api_key(API_KEY)
+
+products <- retailer_products(28)
+
+# Hit API moderately hard.
+#
+library(purrr)
+#
+products %>%
+  pull(product_id) %>%
+  map(product_prices)
+
+# Hit API pretty hard (parallel calls).
+#
+library(furrr)
+#
+options(future.supportsMulticore.unstable = "quiet")
+plan(multisession)
+#
+products %>%
+  pull(product_id) %>%
+  future_map(product_prices)
