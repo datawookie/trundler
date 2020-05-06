557165a56f35ed6635201980e7f37a5998889994
Andrew B. Collier <andrew@exegetic.biz>
2020-05-06 04:27:30 +0000

    Add test for stats_daily; fix column tests

---
+++ tests/testthat/test-4-stats.R
@@ -0,0 +1,7 @@
+context("stats")
+
+test_that("stats daily (correct columns)", {
+  stats_daily <- admin_stats_daily()
+
+  expect_equal(names(stats_daily), c("date", "retailer_id", "count", "price_sum", "price_avg", "price_std", "price_min", "price_max"))
+})
