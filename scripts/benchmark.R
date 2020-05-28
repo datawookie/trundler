1bc74bdc7accb69a2b801a2789e155a56aeb2b66
Andrew B. Collier <andrew@exegetic.biz>
2020-05-28 05:14:11 +0000

    Update for new package name

--- scripts/benchmark.R
+++ scripts/benchmark.R
@@ -1,14 +1,28 @@
-library(retail)
+library(trundler)
 library(dplyr)
 library(microbenchmark)
 
-API_KEY = Sys.getenv("RETAIL_KEY")
-
+API_KEY = Sys.getenv("TRUNDLER_KEY")
 set_api_key(API_KEY)
 
+# Some "quick" queries.
+#
+microbenchmark(
+  retailer(),
+  retailer(1),
+  times = 100
+)
+
+# Test batch size.
+#
 microbenchmark(
   retailer_products(9, limit = 100),
   retailer_products(9, limit = 1000),
   retailer_products(9, limit = 10000),
   times = 10
-  )
+)
+
+microbenchmark(
+  retailer_products(10, limit = 10000),
+  times = 10
+)
