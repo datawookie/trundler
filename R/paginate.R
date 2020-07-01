1c077a30f936dcf45aed065df340f60d133398c2
Megan Beckett <megan.beckett13@gmail.com>
2020-07-01 07:24:25 +0200

    HEAD functions for endpoints for price and product (#33)
    * Add HEAD wrapper
    
    * Add HEAD argument
    
    * Add head request functionality
    
    * Move head argument
    
    * Add head argument
    
    * Add message and head argument
    
    * Add head request to product_prices
    
    * Modify description
    
    * Add head argument to retailer_products()
    
    * Don't worry about status 204 for HEAD
    
    * Tweak documentation
    
    * Return integer for HEAD (rather than numeric)
    
    * Only give warning if not retrieving HEAD
    
    * Renaming
    
    * Add tests for remaining HEAD calls
    
    Co-authored-by: Andrew B. Collier <andrew@exegetic.biz>

--- R/paginate.R
+++ R/paginate.R
@@ -12,52 +12,61 @@
 #'
 #' @param url API endpoint
 #' @param limit Number of items per query
+#' @param head Return the data (\code{FALSE}) or the number of records (\code{TRUE})?
 #' @param verbose Whether to produce verbose output.
-paginate <- function(url, limit = 10000, verbose = FALSE) {
+paginate <- function(url, head = FALSE, limit = 10000, verbose = FALSE) {
   offset = 0
   results <- list()
 
-  while(TRUE) {
-    url <- url %>%
-      param_set(key = "limit", value = limit) %>%
-      # Prevent converting to scientific notation because "+" has different meaning in URL.
-      param_set(key = "offset", value = format(offset, scientific = FALSE))
+  if (!head) {
+    while(TRUE) {
+      url <- url %>%
+        param_set(key = "limit", value = limit) %>%
+        # Prevent converting to scientific notation because "+" has different meaning in URL.
+        param_set(key = "offset", value = format(offset, scientific = FALSE))
 
-    response <- GET(url)
+      response <- GET(url)
 
-    # Check for "204 NO CONTENT".
-    #
-    if (response$status_code == 204) break
+      # Check for "204 NO CONTENT".
+      #
+      if (response$status_code == 204) break
+
+      check_response_error(response)
+      check_response_json(response)
+
+      result <- response %>%
+        content(as = "text", encoding = "UTF-8") %>%
+        fromJSON()
+
+      if (verbose) {
+        message(sprintf("Retrieved %d results.", nrow(result)))
+      }
+
+      if (!length(result)) {
+        if (verbose) message("No more rows.")
+        break
+      }
+
+      results <- c(results, list(result))
+
+      if (nrow(result) < limit) {
+        if (verbose) message("Received fewer rows than requested.")
+        break
+      }
+
+      offset = offset + limit
+    }
+
+    do.call(rbind, results) %>%
+      as_tibble() %>%
+      mutate_at(vars(any_of(c("brand", "model", "sku"))), as.character) %>%
+      mutate_at(vars(any_of(c("price", "price_promotion"))), as.numeric) %>%
+      mutate_at(vars(any_of(c("time"))), function(time) {as.POSIXct(time, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC")})
+  } else {
+    response <- HEAD(url)
 
     check_response_error(response)
-    check_response_json(response)
 
-    result <- response %>%
-      content(as = "text", encoding = "UTF-8") %>%
-      fromJSON()
-
-    if (verbose) {
-      message(sprintf("Retrieved %d results.", nrow(result)))
-    }
-
-    if (!length(result)) {
-      if (verbose) message("No more rows.")
-      break
-    }
-
-    results <- c(results, list(result))
-
-    if (nrow(result) < limit) {
-      if (verbose) message("Received fewer rows than requested.")
-      break
-    }
-
-    offset = offset + limit
+    as.integer(response$headers$`x-total-count`)
   }
-
-  do.call(rbind, results) %>%
-    as_tibble() %>%
-    mutate_at(vars(any_of(c("brand", "model", "sku"))), as.character) %>%
-    mutate_at(vars(any_of(c("price", "price_promotion"))), as.numeric) %>%
-    mutate_at(vars(any_of(c("time"))), function(time) {as.POSIXct(time, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC")})
 }
--- tests/testthat/test-3-product.R
+++ tests/testthat/test-3-product.R
@@ -8,17 +8,6 @@ test_that("product", {
   expect_equal(names(first_product), c("product_id", "retailer_id", "product_url", "product", "brand", "model", "sku", "barcodes"))
 })
 
-test_that("product prices (call succeeds)", {
-  expect_error(product_prices(product_id), NA)
-  expect_error(product_prices(product_id, verbose = TRUE), NA)
-})
-
-test_that("product prices (correct columns)", {
-  first_product_prices <- product_prices(product_id)
-
-  expect_equal(names(first_product_prices), c("product_id", "time", "price", "price_promotion", "price_effective", "available"))
-})
-
 test_that("brand is NULL", {
   product_null_brand <- product(product_id_null_brand)
 
@@ -37,4 +26,11 @@ test_that("product name has apostrophe", {
 
 test_that("brand name has apostrophe", {
   expect_error(products(brand = "Jerry's"), NA)
+})
+
+test_that("product count", {
+  product_count <- products(head = TRUE)
+
+  expect_type(product_count, "integer")
+  expect_length(product_count, 1)
 })
