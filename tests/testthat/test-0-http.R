20096b956ac4497c47723cfd406cd99053a65afd
Andrew B. Collier <andrew@exegetic.biz>
2020-07-19 06:21:42 +0000

    Use /category endpoint (doesn't require API key)

--- tests/testthat/test-0-http.R
+++ tests/testthat/test-0-http.R
@@ -3,7 +3,7 @@ context("http")
 library(httr)
 
 test_that("check that response is compressed", {
-  r = trundler:::GET("https://api.trundler.dev/retailer")
+  r = trundler:::GET("https://api.trundler.dev/category")
 
   expect_equal(headers(r)$`content-encoding`, "gzip")
 })
