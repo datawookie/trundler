6f322f338f3ac3cde4dc4df6334aae1b759ae0ca
Andrew Collier <andrew@exegetic.biz>
2020-07-28 17:50:54 +0200

    Handle issues with {progressr}
    * Don't set handlers in .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -3,18 +3,11 @@
 }
 
 .onLoad <- function(libname, pkgname) {
-  # Handlers for {progressr}.
-  #
-  handlers(list(
-    handler_progress(
-      format   = ":spin :current/:total [:bar] :percent in :elapsed ETA: :eta",
-      width    = 80,
-      complete = "+"
-    )
-  ))
-
   invisible()
 }
 
 .onUnload <- function(libname, pkgname) {
+}
+
+.onDetach <- function(libname, pkgname) {
 }
