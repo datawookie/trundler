e2d763689e9a3d0488f28efd9b32547d33274114
Andrew B. Collier <andrew@exegetic.biz>
2020-09-26 15:52:00 +0000

    Move palette to data

--- data-raw/palette.R
+++ data-raw/palette.R
@@ -1,12 +1,17 @@
-#' @export
 TRUNDLER_ORANGE = "#FB792B"
-#' @export
 TRUNDLER_BLUE   = "#106682"
-#' @export
 TRUNDLER_GREY   = "#AAAAAA"
-#' @export
 TRUNDLER_GREEN  = "#5D9D3F"
-#' @export
 TRUNDLER_RED    = "#EB284B"
-#' @export
 TRUNDLER_SLATE  = "#708090"
+
+usethis::use_data(
+  TRUNDLER_ORANGE,
+  TRUNDLER_BLUE,
+  TRUNDLER_GREY,
+  TRUNDLER_GREEN,
+  TRUNDLER_RED,
+  TRUNDLER_SLATE,
+  internal = FALSE,
+  overwrite = TRUE
+)
