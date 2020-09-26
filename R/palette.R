c1da8edbe9e6b93e9d304850b6ac175dff0d9a40
Andrew B. Collier <andrew@exegetic.biz>
2020-09-26 15:16:01 +0000

    Add colour palette

---
+++ R/palette.R
@@ -0,0 +1,12 @@
+#' @export
+TRUNDLER_ORANGE = "#FB792B"
+#' @export
+TRUNDLER_BLUE   = "#106682"
+#' @export
+TRUNDLER_GREY   = "#AAAAAA"
+#' @export
+TRUNDLER_GREEN  = "#5D9D3F"
+#' @export
+TRUNDLER_RED    = "#EB284B"
+#' @export
+TRUNDLER_SLATE  = "#708090"
