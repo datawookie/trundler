1b7db84b9ca5e030685d4c2ec1665c5b1922382e
Matt Dennis <56922571+matt-denni@users.noreply.github.com>
2021-01-04 13:38:26 +0200

    Fixes for price interpolate (#45)
    * Removed code from report, restructured report text, and included gt tables
    * Initial work on price_complete()
    * Use product_id as input argument.  Check that dates are in the correct format.
    * Import {tidyr} and update documentation.
    * Added tests for price_complete()
    * Tidyr is an import, not a suggest
    * Prevent price_complete() from failing when date range is unspecified.
    * Add comment for examples.
    * Only import complete() from tidyr
    * Add is_interpolated and time as global variables.
    * Update documentation
    * Import full tidyr package - need more than just complete()
    * Reduce white space, reduce unnecessary variables, split assigning of 'is_interpolate' and the interpolated price.
    * Check value of 'is_interpolate'.
    * Clip date range when date_min or date_max falls inside range of dates for product price history.
    * Test specified min and max dates when greater or less than default min and max dates.
    * Consolidate prices when there are multiple prices per day.
    * Add dependencies required for {textshaping}.
    * Add libgit2-dev
    * Don't hard code max date; filter results to specified date range after interpolation.

--- tests/testthat/test-8-price-complete.R
+++ tests/testthat/test-8-price-complete.R
@@ -1,8 +1,10 @@
 context("price_complete")
 
+MAX_DATE = as.Date(max(product_prices(product_id)$time)) + 10
+
 test_that("price_complete", {
   skip_on_cran()
-  first_product <- price_complete(product_id, "2020-01-01", "2020-12-31")
+  first_product <- price_complete(product_id, "2020-01-01", MAX_DATE)
   expect_equal(names(first_product), c("product_id", "date", "price", "is_interpolated"))
 })
 
@@ -11,7 +13,7 @@ test_that("default min date is correct", {
   first_date_no_interpolation <- tail(as.Date(product_prices(product_id)$time), n = 1)
   first_date_with_interpolation <- price_complete(product_id,
                                                      date_min = NULL,
-                                                     date_max = "2020-12-31")$date[1]
+                                                     date_max = MAX_DATE)$date[1]
   expect_equal(first_date_no_interpolation, first_date_with_interpolation)
 })
 
@@ -34,20 +36,36 @@ test_that("valid max date format", {
   expect_error(price_complete(product_id, date_min = NULL, date_max = "20201231"))
 })
 
-test_that("correct minimum date", {
+test_that("correct minimum date when date range extended", {
   skip_on_cran()
   date_min <- price_complete(product_id,
-                             date_min = "2020-01-01",
-                             date_max = "2020-12-31")$date[1]
-  expect_equal(date_min, as.Date("2020-01-01"))
+                             date_min = "2019-01-01",
+                             date_max = MAX_DATE)$date[1]
+  expect_equal(date_min, as.Date("2019-01-01"))
 })
 
-test_that("correct maximum date", {
+test_that("correct minimum date when date range abbreviated", {
+  skip_on_cran()
+  date_min <- price_complete(product_id,
+                             date_min = "2020-10-01",
+                             date_max = "2020-10-10")$date[1]
+  expect_equal(date_min, as.Date("2020-10-01"))
+})
+
+test_that("correct maximum date when date range extended", {
   skip_on_cran()
   date_max <- tail(price_complete(product_id,
                              date_min = "2020-01-01",
-                             date_max = "2020-12-31")$date, n = 1)
-  expect_equal(date_max, as.Date("2020-12-31"))
+                             date_max = MAX_DATE)$date, n = 1)
+  expect_equal(date_max, as.Date(MAX_DATE))
+})
+
+test_that("correct maximum date when date range abbreviated", {
+  skip_on_cran()
+  date_max <- tail(price_complete(product_id,
+                                  date_min = "2020-10-01",
+                                  date_max = "2020-10-10")$date, n = 1)
+  expect_equal(date_max, as.Date("2020-10-10"))
 })
 
 test_that("is_interpolated", {
