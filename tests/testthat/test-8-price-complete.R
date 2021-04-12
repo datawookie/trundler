e1d310d1d3fa5d0fc48dc331df6a2039a03d3eea
Matt Dennis <mattdenni@gmail.com>
2021-04-12 13:17:57 +0200

    fix: Order dates returned by product_prices() and price_complete() to ensure that first and last dates are correct.

--- tests/testthat/test-8-price-complete.R
+++ tests/testthat/test-8-price-complete.R
@@ -10,19 +10,34 @@ test_that("price_complete", {
 
 test_that("default min date is correct", {
   skip_on_cran()
-  first_date_no_interpolation <- tail(as.Date(product_prices(product_id)$time), n = 1)
-  first_date_with_interpolation <- price_complete(product_id,
-                                                     date_min = NULL,
-                                                     date_max = MAX_DATE)$date[1]
+  first_date_no_interpolation <- as.Date(product_prices(product_id)$time) %>%
+    sort(decreasing = FALSE) %>%
+    first()
+
+  first_date_with_interpolation <- price_complete(
+    product_id,
+    date_min = NULL,
+    date_max = MAX_DATE
+  )$date %>%
+    sort(decreasing = FALSE) %>%
+    first()
+
   expect_equal(first_date_no_interpolation, first_date_with_interpolation)
 })
 
 test_that("default max date is correct", {
   skip_on_cran()
-  last_date_no_interpolation <- as.Date(product_prices(product_id)$time)[1]
-  last_date_with_interpolation <- tail(price_complete(product_id,
-                                                  date_min = "2020-01-01",
-                                                  date_max = NULL)$date, n = 1)
+  last_date_no_interpolation <- as.Date(product_prices(product_id)$time) %>%
+    sort() %>%
+    last()
+
+  last_date_with_interpolation <- price_complete(
+    product_id,
+    date_min = "2020-01-01",
+    date_max = NULL)$date %>%
+    sort() %>%
+    last()
+
   expect_equal(last_date_no_interpolation, last_date_with_interpolation)
 })
 
@@ -55,8 +70,8 @@ test_that("correct minimum date when date range abbreviated", {
 test_that("correct maximum date when date range extended", {
   skip_on_cran()
   date_max <- tail(price_complete(product_id,
-                             date_min = "2020-01-01",
-                             date_max = MAX_DATE)$date, n = 1)
+                                  date_min = "2020-01-01",
+                                  date_max = MAX_DATE)$date, n = 1)
   expect_equal(date_max, as.Date(MAX_DATE))
 })
 
