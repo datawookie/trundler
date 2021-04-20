a0696b2fc6295987dc73a6ee6aeaf98c4f5f2d96
Andrew Collier <andrew@exegetic.biz>
2021-04-20 12:07:05 +0200

    fix: Correct path to icon SVG

--- inst/hex/logo.R
+++ inst/hex/logo.R
@@ -11,7 +11,7 @@ IMAGE = tempfile(fileext = ".svg")
 #
 IMAGE = path.expand(IMAGE)
 #
-download.file("https://www.trundler.dev/images/icon.svg", IMAGE)
+download.file("https://www.trundler.dev/img/icon.svg", IMAGE)
 
 LOGO_PATH = here("man/figures/")
 LOGO_FILE = file.path(LOGO_PATH, "logo.png")
